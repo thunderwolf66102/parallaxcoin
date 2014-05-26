88ce41fdecdf7e4ff863bfe337345ffa60087780
David Hou <david.hou314@gmail.com>
2014-05-25 22:33:44 -0400

    Add back chaosagent.org seed

--- src/net.cpp
+++ src/net.cpp
@@ -1192,7 +1192,7 @@ void MapPort(bool)
 // The first name is used as information source for addrman.
 // The second name should resolve to a list of seed addresses.
 static const char *strMainNetDNSSeed[][2] = {
-//    {"chaosagent.org", "chaosagent.org"},
+    {"chaosagent.org", "chaosagent.org"},
     {"dnsseed.parallaxcoin.com", "dnsseed.parallaxcoin.com"},
     {NULL, NULL}
 };
